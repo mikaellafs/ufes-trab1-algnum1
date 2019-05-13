b36dd3a8209e7435df5fd413913edf5ee704d6a3
Gabrielk99 <34044829+Gabrielk99@users.noreply.github.com>
2019-05-12 21:07:49 -0300

    MENU
    eu to amando esse menuzinho aaaaaa

---
+++ main.m
@@ -0,0 +1,36 @@
+## Copyright (C) 2019 gabri
+## 
+## This program is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} main (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: gabri <gabri@LAPTOP-GDFUG9L9>
+## Created: 2019-05-12
+
+function retval = main (input1, input2)
+  continuar = 1;
+  while(continuar)
+    escolha = menu("Escolha o que quer resolver","Exercicio_1","Exercicio_2");
+    switch escolha
+      case 1 
+       source("trab.m")
+    endswitch
+    continuar = yes_or_no("Quer continuar??");
+  end 
+endfunction
